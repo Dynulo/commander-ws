fe7196b12042c0416816e8dc1e59a68b2a8d3427
Prince <mandeep@mandeepsingh.ca>
2022-06-11 19:01:05 +0000

    add serde

--- commander-ws-model/src/lib.rs
+++ commander-ws-model/src/lib.rs
@@ -1,10 +1,13 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Root {
     Version,
     Unauthenticated(Unauthenticated),
 
 }
 
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Unauthenticated {
     Login(String),
 }
-
